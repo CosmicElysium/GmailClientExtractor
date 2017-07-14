e6d0d36b312d58470ea133518726204c5173b7f1
CosmicElysium <sivisports@msn.com>
2017-07-14 20:35:31 +0200

    Used pandas library to extract table from HTML in email containing list of clients

--- clientextractor.py
+++ clientextractor.py
@@ -13,7 +13,10 @@ import sys
 import urllib
 import json
 import re
-import time
+import datetime
+
+import calendar
+from pandas import read_html
 
 from optparse import OptionParser
 
@@ -31,14 +34,41 @@ SCOPE= 'https://mail.google.com/'
 
 ENDOFHEADER= "Number\r\n"
 
-class Client
+MONTH_ABBR_NUMBERS = {v + ".": k for k,v in enumerate(calendar.month_abbr)}
+MONTH_NUMBERS = {v: k for k,v in enumerate(calendar.month_name)}
+
+
+CURRENTYEAR = 2017
+
+class Client:
     
-    def setDateLastUpdated:
-        pass
+    def __init__(self, ref_number, update_datetime, created_datetime, firstName, 
+            lastName, email, airlines, flight_number, origin, arrival_datetime ):
+        self.ref_number= ref_number
+        self.update_datetime= update_datetime
+        self.created_datetime= created_datetime
+        self.firstName= firstName
+        self.lastName= lastName
+        self.email= email
+        self.airlines= airlines
+        self.flight_number= flight_number
+        self.origin= origin
+        self.arrival_datetime= arrival_datetime
 
-    def setDateCreated:
-        pass
+    def setDateTimeLastUpdated(self, year, month, day, hour, minute):
+        self.dateTimeUpdated = datetime.datetime(year, month, day, hour, minute)
 
+    def setDateTimeCreated(self, year, month, day, hour, minute):
+        self.dateTimeCreated = datetime.datetime(year, month, day, hour, minute)
+
+    def setReferenceNumber(self, refNumber):
+        self.referenceNumber = refNumber
+
+    def setFirstName(self, firstName):
+        self.firstName = firstName
+
+    def setLastName(self, lastName):
+        self.lastName = lastName
 
 
 class ClientExtractor:
@@ -64,23 +94,92 @@ class ClientExtractor:
         emailData= EmailTools.ConvertEmailMessageToData(latestEmail, 0)
         self.dataList = DataTools.BreakDataStringToDataList(emailData)
 
-    def ConvertListToClients
-        for eachStudent in self.dataList:
-            entryDates = re.search('([0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]).*?([0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9])', eachStudent)
-            dateUpdated = entryDates.group(1)
-            dateCreated = entryDates.group(2)
-            
+    def GetFakeRawClientList(self):
+        with open("./data/EmailData2.txt",'r') as emailData:
+            self.dataList = emailData.read()
+
+    def ConvertListToClients(self):
+        
+        month = int(raw_input('Enter Month #: '))
+        day = int(raw_input('Enter Day #: '))
+
+        dateToGet = datetime.date(CURRENTYEAR, month, day)
+        
+        self.clientSet = DataTools.HtmlStringToClientList(self.dataList, dateToGet)
 
 
 class DataTools:
+
+    @staticmethod
+    def HtmlStringToClientList(html_string, date):
+            clientList = []
+            htmlParsed = re.split("<tr>|</tr>", html_string)
+            betterParsed = htmlParsed[18:-3:2]
+            for client in betterParsed:
+                currentClient = read_html("<table>" + "<tr>" + client + "</tr>" + "</table>")[0].values.tolist()[0]
+                pickUpDate = currentClient[16]
+                ###print pickUpDate
+                if pickUpDate == u'\xc2':
+                    continue
+                pickUpDateParsed = pickUpDate.split()
+                monthName = pickUpDateParsed[1]
+                if "." in monthName:
+                    pickUpDateObject = datetime.date(int(pickUpDateParsed[2]), MONTH_ABBR_NUMBERS[monthName], int(pickUpDateParsed[0]))
+                else:
+                    pickUpDateObject = datetime.date(int(pickUpDateParsed[2]), MONTH_NUMBERS[monthName], int(pickUpDateParsed[0]))
+                if pickUpDateObject == date:
+                    pickUpTime = MiscTools.TimeStringToTimeObject(currentClient[17])
+                    pickUpDateTime = datetime.datetime(pickUpDateObject.year, pickUpDateObject.month, pickUpDateObject.day, pickUpTime.hour, pickUpTime.minute)
+                    newClient = Client(currentClient[1], MiscTools.DateTimeStringToDateTimeObjects(currentClient[2]),
+                            MiscTools.DateTimeStringToDateTimeObjects(currentClient[3]), currentClient[4], currentClient[5],
+                            currentClient[6], currentClient[12], currentClient[13], currentClient[14], pickUpDateTime)
+                    clientList.append(newClient)
+            return clientList
+
 
     @staticmethod
     def BreakDataStringToDataList(dataString):
         dataList = re.split('[0-9][0-9][0-9][0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9][0-9][0-9]', dataString)
         return dataList
 
+    @staticmethod
+    def SplitFirstWordOffString(string_to_split):
+        string_to_split = string_to_split.lstrip()
+        first_word = string_to_split.split(" ")[0]
+        new_string = string_to_split.replace(first_word,"")
+        new_string = new_string.lstrip()
+        return first_word, new_string
+
 class MiscTools:
     
+    @staticmethod
+    def DateTimeStringToDateTimeObjects(dateTimeString):
+        #print dateTimeString
+        date, time = dateTimeString.split('  ')
+        day,month,year = date.split('/')
+        timeNumber, amPm  = time.split(' ')
+        hour, minute = timeNumber.split('.')
+        if amPm == u"PM":
+            hour = int(hour) + 12
+            if hour == 24:
+                hour = 0
+        return datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))
+
+    @staticmethod
+    def TimeStringToTimeObject(timeString):
+        hour = int(timeString[0:2])
+        if timeString[3:5] == '':
+            minute = 0
+        else:
+            minute = int(timeString[3:5])
+        if 'PM' in timeString :
+            hour = hour + 12
+        return datetime.time(hour, minute)
+
+    @staticmethod
+    def DatesAreCloseEnough(date1, date2, distanceInDays):
+        pass
+
     @staticmethod
     def GetGmailCreds(path_to_data_file):
         credentials = {}
@@ -114,7 +213,7 @@ class EmailTools:
 #TODO:save text files of both raw emails to avoid data cap
     @staticmethod
     def ConvertEmailMessageToData(email_message, payload_index):
-	emailPayload= email_message.get_payload(payload_index)
+        emailPayload= email_message.get_payload(payload_index)
         dataDecodeable= emailPayload.get_payload(decode= True)
         dataDecoded= dataDecodeable.decode('utf-8')
         return dataDecoded
@@ -231,17 +330,6 @@ class OAuth2Tools:
         """
         params = {}
         params['client_id'] = client_id
-        params['client_secret'] = client_secret
-        params['refresh_token'] = refresh_token
-        params['grant_type'] = 'refresh_token'
-        request_url = OAuth2Tools.AccountsUrl('o/oauth2/token')
-
-        response = urllib.urlopen(request_url, urllib.urlencode(params)).read()
-        return json.loads(response)
-
-
-    @staticmethod
-    def GenerateOAuth2String(username, access_token, base64_encode=True):
         """Generates an IMAP OAuth2 authentication string.
 
         See https://developers.google.com/google-apps/gmail/oauth2_overview
